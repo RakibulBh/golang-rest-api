fc325c3374ce242f0a76fb9d4f3459e338501b6a
RakibulBh <rakibulbhuiyan1973@gmail.com>
2025-02-15 14:25:01 +0000

    add updating event functionality

--- routes/events.go
+++ routes/events.go
@@ -69,10 +69,18 @@ func updateEvent(context *gin.Context) {
 
 	var updatedEvent models.Event
 
-	err := context.ShouldBindJSON(&updatedEvent)
+	err = context.ShouldBindJSON(&updatedEvent)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
 		return
 	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully."})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -6,5 +6,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent) // events/1, events/5
 	server.POST("/events", createEvent)
-	server.PUT("/events/:id") // events/1, events/5
+	server.PUT("/events/:id", updateEvent) // events/1, events/5
 }
